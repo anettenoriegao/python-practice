f56abaa510abcc51bc3f74c6cedb07e17f89641c
Anette Noriega <anoriegaocano@gmail.com>
2025-08-12 17:24:07 -0400

    added answers

---
+++ exercise-variables.py
@@ -0,0 +1,30 @@
+# 1
+onomatopoeia = "pop"
+print(type(onomatopoeia))
+
+# 2
+lyrics = """
+Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:
+"""
+
+#3
+gpa = 3.75
+print(gpa)
+print(type(gpa))
+
+#4
+empty_var = None
+print(empty_var)
+print(type(empty_var))
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,24 @@
+# Set up our variables
+num1 = 3.14
+num2 = 2
+name = "Mike"
+num3 = 16.00
+
+# Create some expressions and store their results
+add_result = num1 + num2
+subtract_check = num2 - num1
+divide_check = num3 / num2
+exponent_result = num3 ** num2
+remainder_result = num3 % num2
+greater_than_check = num1 > num2
+multiple_ops_check = round((divide_check * 5) ** num1, 2)
+
+# Print results
+
+print(add_result)
+print(subtract_check)
+print(divide_check)
+print(exponent_result)
+print(remainder_result)
+print(greater_than_check)
+print(multiple_ops_check)
