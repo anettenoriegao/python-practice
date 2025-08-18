92908c52506b0a8bab8a2c11318a2e33495a4d62
Anette Noriega <anoriegaocano@gmail.com>
2025-08-18 13:27:05 -0400

    updated restauraunt-ordering.py

--- restaurant-ordering.py
+++ restaurant-ordering.py
@@ -11,27 +11,24 @@ pizza_price = 8.49
 salad_price = 4.99
 drink_price = 1.99
 
-# Define dictionary (collection) - Menu - item + price
-menu_items = {
-    "1": ("Burger", burger_price),
-    "2": ("Pizza",  pizza_price),
-    "3": ("Salad",  salad_price),
-    "4": ("Drink",  drink_price),
-}
-
-# Create collection for order/cart (we won't use it yet—STOP HERE step)
-order = {}           # e.g., {"Burger": 2}
-total_cost = 0.0     # running total (used later)
-
 # # display welcome
 # PRINT statement - WELCOME TO RESTAURANT
 print("WELCOME TO RESTAURANT")
 
 # Initiate while loop
+order_total = 0
 while True:
     # display menu items (plain prints)
     print("Item | Name   | Price")
     print("---------------------")
     print("1    | Burger | $5.99")
     print("2    | Pizza  | $8.49")
-    print("3    | Salad  | $8.49")
+    print("3    | Salad  | $4.99")
+    print("4    | Drink  | $1.99")
+
+    # prompt menu selection
+    item_choice = input("Choose item:")
+
+    #promp qty
+    item_quantity = int(input("How many:"))
+    
