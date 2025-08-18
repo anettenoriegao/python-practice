86f9119d3fdb5e8dce86d8114b72d58b5aeaac1f
Anette Noriega <anoriegaocano@gmail.com>
2025-08-18 16:12:06 -0400

    updated again

--- restaurant-ordering.py
+++ restaurant-ordering.py
@@ -1,44 +1,29 @@
-# Define variables
-# Item number  name    price
-# 1            Burger  $5.99
-# 2            Pizza   $8.49
-# 3            Salad   $4.99
-# 4            Drink   $1.99
 
-# Prices of menu options
-burger_price = 5.99
-pizza_price = 8.49
-salad_price = 4.99
-drink_price = 1.99
-
-# # display welcome
 # PRINT statement - WELCOME TO RESTAURANT
 print("WELCOME TO RESTAURANT")
 
 menu = {
-    'Burger' : 5.99,
-    'Pizza' : 8.49,
-    'Salad' : 4.99,
-    'Drink' : 1.99,
-}
-item_number = 1
-for item, price in menu.items():
-    print(f'{item_number}, {item}, ${price}')
-    item_number += 1
-    
+       1 : {"name":"Burger","price": 5.99},
+       2 : {"name":"Pizza","price": 8.49},
+       3 : {"name":"Salad","price": 4.99},
+       4 : {"name":"Drink","price": 1.99},
+    }
+
 # Initiate while loop
 order_total = 0
-while True:
-    # display menu items (plain prints)
-    print("Item | Name   | Price")
-    print("---------------------")
-    print("1    | Burger | $5.99")
-    print("2    | Pizza  | $8.49")
-    print("3    | Salad  | $4.99")
-    print("4    | Drink  | $1.99")
+item_number = 1
 
-    # prompt menu selection
-    item_choice = input("Choose item:")
 
-    #promp qty
-    item_quantity = int(input("How many:"))
+print("Menu")
+for item_no, details in menu.items():
+      print(f'{item_no}. {details["name"]} - ${details['price']}')
+
+# prompt menu selection
+    # Prompt menu selection (user input)
+item_choice = input("Choose item number: ")
+
+    # prompt qty
+    # Prompt qty
+item_qty = int(input("How many: "))
+
+
