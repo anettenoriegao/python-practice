df1e150d343dede2346b265dd31b885ad4f3ea82
Anette Noriega <anoriegaocano@gmail.com>
2025-08-18 12:55:12 -0400

    Created restaurant-ordering-system.py

---
+++ restaurant-ordering-system.py
@@ -0,0 +1,22 @@
+
+# define variables
+
+# Item number name of item price
+# 1 Burger $5.99
+# 2 Pizza $8.49
+# 3 Salad $4.99
+# 4 Drink $1.99
+
+# display welcome
+
+# # display menu items
+# display menu items - (print statement) - for loop initiate menu_items
+print("Item | Name | Price")
+print()
+
+
+
+# display menu items
+
+# prompt menu selection
+
